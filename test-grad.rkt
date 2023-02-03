f9c58bd632fab6c6fd1f2cc3a4449ff82e15e245
Giuseppe Pitton <pitton@protonmail.com>
2023-02-03 22:55:39 +0000

    map-tree macro almost done

--- test-grad.rkt
+++ test-grad.rkt
@@ -61,7 +61,7 @@
 
 
 ;; Tests for the grad macro
-(displayln "grad")
+(displayln "grad ---------------------------------------------------------------")
 (displayln (grad 2 c))
 (displayln (grad x c))
 (displayln (grad c c))
@@ -85,10 +85,13 @@
     ;; args is empty: recursion has ended. Just return the reodered term.
     [(_ (op) (term:number)) #'term]  ;; number specialisation.
     [(_ (op) (term)) #''term]        ;; single variable specialisation.
-    [(_ (op) term) #''term]          ;; general case.
+    [(_ (op) (term ...))             ;; general case.
+     #''(op term ...)]
     ;; Term with a single element: just return the element.
-    [(_ (n:number)) #'n]
+    [(_ (n:number)) #'n]             ;; with brackets
+    [(_ n:number) #'n]               ;; without brackets
     [(_ (s:id)) #''s]                ;; quote the symbol
+    [(_ s:id) #''s]                  ;; without brackets
     ;; Proper term: initialise the recursion.
     [(_ (op arg0 arg1 ...))
      #'(reorder-term (op arg1 ...) (arg0))]
@@ -111,10 +114,11 @@
      #'(reorder-term (op arg1 ...) (t0 t1 ... arg0))]))
 
 
-(displayln "reorder-term")
+(displayln "reorder-term -------------------------------------------------------")
 (displayln (reorder-term (+ 1 2 3 4)))
 (displayln (reorder-term (* 1 2 3 4)))
 (displayln (reorder-term (c)))
+;(displayln (reorder-term ((2))))  ;; this should fail
 (displayln (reorder-term (+ 2 3 c 3)))
 (displayln (reorder-term (+ c 2)))
 (displayln (reorder-term (+ c c 8 c c 4 c 1 c c)))
@@ -153,7 +157,7 @@
     [_ #'"unexpected syntax"]))
 
 
-(displayln "mult->expt")
+(displayln "mult->expt ---------------------------------------------------------")
 (displayln (mult->expt ()))
 (displayln (mult->expt 5))
 (displayln (mult->expt (x)))
@@ -162,27 +166,52 @@
 (displayln (mult->expt (1 x x x)))
 (displayln (mult->expt (6 x x x x x x)))
 
+(define-for-syntax (notlist? x) (not (list? x)))
 
-;; Applies a macro f to each node in a (possibly nested)
-;; S-expression (interpreted as a tree here). This is a
-;; useful tool to simplify a polynomial expression by
-;; applying a term-reordering or a simplifying macro to
-;; each sub-expression.
-#|(define-syntax (map-tree stx)
+;; Applies a macro f to each node in a (possibly nested) S-expression
+;; (interpreted as a tree here). This is a useful tool to simplify a
+;; polynomial expression by applying a term-reordering or a simplifying
+;; macro to each sub-expression.
+(define-syntax (map-tree stx)
   (syntax-case stx ()
-    [(_ f ()) #'()]
-    [(_ f (ex)) #'(f ex)]
+    ;; Empty tree. End of recursion.
+    [(_ f ()) #''()]
+    ;; No element of ex is a list. We can just apply f.
+    [(_ f ex)
+     (andmap notlist? (syntax->datum #'ex))
+     #'(f ex)]
+    ;; (operand list ...) -> (op (recur list) (recur ...))
     [(_ f (op ex0 ex1 ...))
-     (list? #'ex0)
-     #'(op (map-tree f ex0) (map-tree f ex1) ...)]
+     (and (member (syntax->datum #'op) '(+ - * ^))
+          (list? (syntax->datum #'ex0)))
+     #'`(op ,(map-tree f ex0) ,@(map-tree f (ex1 ...)))]
+    ;; (operand symbol ...) -> (op symbol (recur ...))
     [(_ f (op ex0 ex1 ...))
-     #'`(op ,@(f (ex0 ex1 ...)))]))
+     (member (syntax->datum #'op) '(+ - * ^))
+     #'`(op ex0 ,@(map-tree f (ex1 ...)))]
+    ;; Tree that does not start with an operator. This is the recursive
+    ;; case started by one of the two steps above.
+    ;; (list ...) -> ((recur list) (recur ...))
+    [(_ f (ex0 ex1 ...))
+     (list? (syntax->datum #'ex0))
+     #'`(,(map-tree f ex0) ,@(map-tree f (ex1 ...)))]
+    ;; Tree that does not start with an operator. This is also a recursive
+    ;; step, like the one above.
+    ;; (symbol ...) -> (symbol (recur ...))
+    [(_ f (ex0 ex1 ...))
+     #'`(ex0 ,@(map-tree f (ex1 ...)))]
+    [_ #'"unexpected syntax"]))
 
-(displayln "map-tree")
-(displayln (map-tree mult->expt (+ c c)))
+(displayln "map-tree -----------------------------------------------------------")
+(displayln (map-tree reorder-term (2)))
+(displayln (map-tree reorder-term (x)))
+(displayln (map-tree reorder-term (+ x 2)))
+(displayln (map-tree reorder-term (+ 2 c 3)))
+(displayln (map-tree reorder-term (+ (+ 2 3) (+ 3 x x 6))))
 ;; 4x^3 + 2x*x^6 - 2
-(displayln (map-tree mult->expt (+ (* x 4 x x) (* 2 x (^ x 6)) -2)))
-|#
+(displayln (map-tree reorder-term (+ (* x 4 x x) (* 2 x (^ x 6)) -2)))
+(displayln (map-tree reorder-term (+ (* x x) (+ 2 x (+ 3 (* 4 x x 2 x))))))
+
 
 ;; TODO simplify expressions like (* ... 0 ...) -> #'0
 (define-syntax simplify
